b959d196002f29720c4bfb60390144fdcc475093
Yuya Ochiai <yuya0321@gmail.com>
2018-12-19 23:34:45 +0900

    Cherry-pick change to get all MAC builds

--- scripts/cp_artifacts.sh
+++ scripts/cp_artifacts.sh
@@ -9,6 +9,7 @@ cp "${SRC}/mattermost-${VERSION}-win-ia32.zip" "${DEST}/mattermost-desktop-${VER
 cp "${SRC}/mattermost-${VERSION}-win-x64.zip" "${DEST}/mattermost-desktop-${VERSION}-win64.zip"
 cp "${SRC}/squirrel-windows/mattermost-setup-${VERSION}.exe" "${DEST}/mattermost-setup-${VERSION}-win64.exe"
 cp "${SRC}/squirrel-windows-ia32/mattermost-setup-${VERSION}-ia32.exe" "${DEST}/mattermost-setup-${VERSION}-win32.exe"
+cp "${SRC}"/mattermost-desktop-*-mac.* "${DEST}/"
 cp "${SRC}"/mattermost-desktop-*.zip "${DEST}/"
 cp "${SRC}"/*.tar.gz "${DEST}/"
 cp "${SRC}"/*.deb "${DEST}/"
